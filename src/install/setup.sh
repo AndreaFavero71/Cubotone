e6290cc17fcf0c3eac9ff224b40820c7faaea774
Andrea <108091411+AndreaFavero71@users.noreply.github.com>
2022-09-11 10:51:25 +0200

    Update setup.sh

--- src/install/setup.sh
+++ src/install/setup.sh
@@ -48,7 +48,6 @@ source .virtualenvs/bin/activate
 print_header "Installing required python packages"
 pip3 install numpy==1.21.4
 pip3 install "picamera[array]"
-pip3 install st7735==0.0.4.post1
 pip3 install RubikTwoPhase==1.0.9
 pip3 install getmac==0.8.3
 
@@ -65,9 +64,9 @@ systemctl enable pigpiod
 
 set +e
 
-print_header "Configuring vnc server to run at startup and prepare setup for cubotino"
-if ! crontab -l 2>/dev/null | grep -q "Cubotino_T_bash.sh"; then
-    (crontab -l 2>/dev/null; echo 'MAILTO=""'; echo @reboot su - pi -c \"/usr/bin/vncserver :0 -geometry 1280x720\"; echo '#@reboot /bin/sleep 5; bash -l /home/pi/cubotino/src/Cubotino_T_bash.sh > /home/pi/cubotino/src/Cubotino_T_terminal.log 2>&1') | crontab -
+print_header "Configuring vnc server to run at startup and prepare setup for cubotone"
+if ! crontab -l 2>/dev/null | grep -q "Cubotone_bash.sh"; then
+    (crontab -l 2>/dev/null; echo 'MAILTO=""'; echo @reboot su - pi -c \"/usr/bin/vncserver :0 -geometry 1280x720\"; echo '#@reboot /bin/sleep 5; bash -l /home/pi/cubotone/src/Cubotone_bash.sh > /home/pi/cubotone/src/Cubotone_terminal.log 2>&1') | crontab -
 fi
 
 print_header "Reboot now? (y/n)"
